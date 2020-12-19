018f0ec171ce6afce4445cf5dd1c7e28cfeffefc
elmaghallawy <elmaghallawy@protonmail.com>
2020-12-19 17:18:02 +0200

    listen to a tcp connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    // listen to 127.0.0.1:7878 for incoming tcp streams
+    // print "Connection established!" when it gets a stream
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => println!("connection established"),
+            Err(e) => println!("Opss error {}", e),
+        }
+    }
 }
