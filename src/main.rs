1bb035621e24c113ec8e054ad522fb322bdd4513
elmaghallawy <elmaghallawy@protonmail.com>
2020-12-21 14:07:53 +0200

    send HTML response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::prelude::*; // contains many traits that let us read from and write to streams
 use std::net::{TcpListener, TcpStream};
 
@@ -19,6 +20,8 @@ fn main() {
 // time we ask for data therefore we made it mutable because its internal state
 // might change
 fn handle_connection(mut stream: TcpStream) {
+    // PARSE REQUEST
+
     // declare buffer (512 bytes) on the stack to hold data that's read in
     let mut buffer = [0; 512];
     // read the stream bytes and puts the data into the buffer
@@ -26,8 +29,17 @@ fn handle_connection(mut stream: TcpStream) {
     // convert bytes in the buffer to a string and print that string
     println!("Request: {}", String::from_utf8_lossy(&buffer[..])); // lossy replace invalid utf-8 squence
 
-    // send response
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let get = b"GET / HTTP/1.1\r\n";
+    // SEND RESPONSE
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}", status_line, contents);
+
     // convert string data to bytes and
     // write takes those bytes and send them directly down the connection
     stream.write(response.as_bytes()).unwrap();
